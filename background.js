f289c36902b8404e0685ca1224a282ce0a9560a1
rcmtcristian <rcmtcristian@gmail.com>
2023-09-22 02:15:59 -0400

    ✨ Feat: it worksssss!!!
    have to update the themes cause now i dont like half of them

--- background.js
+++ background.js
@@ -1,4 +1,28 @@
-/* Getting the value of the radio button that is checked. */
+// /* Getting the value of the radio button that is checked. */
+// // const colorArrays = [
+// //   [
+// //     "rgb(22, 27, 34)",
+// //     "rgb(99, 28, 3)",
+// //     "rgb(189, 86, 29)",
+// //     "rgb(250, 122, 24)",
+// //     "rgb(253, 223, 104)",
+// //   ],
+// //   [
+// //     "rgb(255, 238, 74)",
+// //     "rgb(255, 197, 1)",
+// //     "rgb(254, 150, 0)",
+// //     "rgb(3, 0, 28)",
+// //     "rgb(255, 0, 255)",
+// //   ],
+// //   [
+// //     "rgb(0, 0, 0)",
+// //     "rgb(127, 127, 127)",
+// //     "rgb(255, 255, 255)",
+// //     "rgb(200, 200, 200)",
+// //     "rgb(100, 100, 100)",
+// //   ],
+// // ];
+
 // const colorArrays = [
 //   [
 //     "rgb(22, 27, 34)",
@@ -7,71 +31,91 @@
 //     "rgb(250, 122, 24)",
 //     "rgb(253, 223, 104)",
 //   ],
-//   [
-//     "rgb(255, 238, 74)",
-//     "rgb(255, 197, 1)",
-//     "rgb(254, 150, 0)",
-//     "rgb(3, 0, 28)",
-//     "rgb(255, 0, 255)",
-//   ],
-//   [
-//     "rgb(0, 0, 0)",
-//     "rgb(127, 127, 127)",
-//     "rgb(255, 255, 255)",
-//     "rgb(200, 200, 200)",
-//     "rgb(100, 100, 100)",
-//   ],
 // ];
 
-const colorArrays = [
-  [
-    "rgb(22, 27, 34)",
-    "rgb(99, 28, 3)",
-    "rgb(189, 86, 29)",
-    "rgb(250, 122, 24)",
-    "rgb(253, 223, 104)",
-  ],
-];
+// // (async () => {
+// //   const myTheme = () => {
+// //     chrome.storage.local.get("favoriteColor", (themeValue) => {
+// //       let chosenColor = themeValue.favoriteColor;
 
-const myTheme = () => {
-  chrome.storage.local.get("favoriteColor", (themeValue) => {
-    let chosenColor = themeValue.favoriteColor;
+// //       const themes = {
+// //         standard: colorArrays[0],
+// //         classic: colorArrays[1],
+// //         githubDark: colorArrays[2],
+// //       };
 
-    const themes = {
-      standard: colorArrays[0],
-      classic: colorArrays[1],
-      githubDark: colorArrays[2],
-    };
+// //       const colors = themes[chosenColor] || [];
+// //       themeActivityOverview(colors);
+// //       themeContributionGraph(colors);
+// //     });
+// //   };
+// //   chrome.storage.onChanged.addListener((changes) => {
+// //     if (changes.favoriteColor) {
+// //       myTheme();
+// //     }
+// //   });
 
-    const colors = themes[chosenColor] || [];
-    themeActivityOverview(colors);
-    themeContributionGraph(colors);
-  });
-};
+// //   function themeContributionGraph(colors) {
+// //     console.log(colors, "maybe");
+// //     const { style } = document.querySelector(":root");
 
-chrome.storage.onChanged.addListener((changes) => {
-  if (changes.favoriteColor) {
-    myTheme();
-  }
-});
+// //     colors.forEach((color, i) => {
+// //       style.setProperty(`--color-calendar-graph-day-L${i}-bg`, color);
+// //     });
+// //   }
 
-function themeContributionGraph(colors) {
-  console.log(colors, "maybe");
-  const { style } = document.querySelector(":root");
+// //   function themeActivityOverview(colors) {
+// //     console.log(colors, "maybe");
+// //     const { style } = document.querySelector(".js-highlight-blob");
+// //     const color = colors[colors.length - 1];
+// //     style.fill = color;
+// //     style.stroke = color;
+// //   }
+// //   (function () {
+// //     myTheme();
+// //   });
+// // })();
 
-  colors.forEach((color, i) => {
-    style.setProperty(`--color-calendar-graph-day-L${i}-bg`, color);
-  });
-}
+// (async () => {
+//   function getTheme() {
+//     return new Promise((resolve) => {
+//       chrome.storage.local.get("favoriteColor", (themeValue) => {
+//         resolve(themeValue.favoriteColor);
+//       });
+//     });
+//   }
 
-function themeActivityOverview(colors) {
-  console.log(colors, "maybe");
-  const { style } = document.querySelector(".js-highlight-blob");
-  const color = colors[colors.length - 1];
-  style.fill = color;
-  style.stroke = color;
-}
+//   function applyTheme(colors) {
+//     const { style } = document.querySelector(":root");
 
-(function () {
-  myTheme();
-});
+//     colors.forEach((color, i) => {
+//       style.setProperty(`--color-calendar-graph-day-L${i}-bg`, color);
+//     });
+
+//     const blobStyle = document.querySelector(".js-highlight-blob").style;
+//     const color = colors[colors.length - 1];
+//     blobStyle.fill = color;
+//     blobStyle.stroke = color;
+//   }
+
+//   async function init() {
+//     const theme = await getTheme();
+//     const themes = {
+//       standard: colorArrays[0],
+//       classic: colorArrays[1],
+//       githubDark: colorArrays[2],
+//     };
+
+//     const colors = themes[theme] || [];
+//     applyTheme(colors);
+//   }
+
+//   chrome.storage.onChanged.addListener((changes) => {
+//     if (changes.favoriteColor) {
+//       init();
+//     }
+//   });
+
+//   // Initialize on page load
+//   init();
+// })();
--- content.js
+++ content.js
@@ -1,4 +1,98 @@
 "use strict";
+
+// const colorArrays = [
+//   [
+//     "rgb(22, 27, 34)",
+//     "rgb(99, 28, 3)",
+//     "rgb(189, 86, 29)",
+//     "rgb(250, 122, 24)",
+//     "rgb(253, 223, 104)",
+//   ],
+
+//   [
+//     "rgb(0, 0, 0)",
+//     "rgb(127, 127, 127)",
+//     "rgb(255, 255, 255)",
+//     "rgb(200, 200, 200)",
+//     "rgb(100, 100, 100)",
+//   ],
+// ];
+
+const colorArrays = [
+  ["#ffffff", "#9be9a8", "#40c463", "#30a14e", "#216e39"],
+  ["#ffffff", "#c6e48b", "#7bc96f", "#239a3b", "#196127"],
+  ["#101217", "#003820", "#00602d", "#10983d", "#27d545"],
+  ["#212836", "#631c03", "#bd561d", "#fa7a18", "#fddf68"],
+  ["#ffffff", "#7FFFD4", "#76EEC6", "#66CDAA", "#458B74"],
+  ["#181818", "#646464", "#A5A5A5", "#DDDDDD", "#F6F6F6"],
+  ["#49006a", "#ac0279", "#f669a0", "#faa0ba", "#fcc5c0"],
+  ["#181818", "#263342", " #344E6C", " #416895", " #4F83BF"],
+  ["#2B2C2F", "#34353B", " #6FC1FF", " #19f9d8", " #FF4B82"],
+  ["#800826", " #bc0026", " #ff2e19", "#fc6e32", "#fed976"],
+  ["#ffffff", "#e48bdc", "#ca5bcc", "#a74aa8", "#61185f"],
+  ["#ffffff", "#a1dab4", "#41b6c4", "#2c7fb8", "#253494"],
+  ["#002b36", "#268bd2", "#2aa198", "#b58900", "#d33682"],
+  ["#fdf6e3", " #eee8d5", " #839496", " #586e75", " #073642"],
+];
+
+(async () => {
+  function getTheme() {
+    return new Promise((resolve) => {
+      chrome.storage.local.get("favoriteColor", (themeValue) => {
+        resolve(themeValue.favoriteColor);
+      });
+    });
+  }
+
+  function applyTheme(colors) {
+    const { style } = document.querySelector(":root");
+
+    colors.forEach((color, i) => {
+      style.setProperty(`--color-calendar-graph-day-L${i}-bg`, color);
+    });
+
+    const blobStyle = document.querySelector(".js-highlight-blob").style;
+    const color = colors[colors.length - 1];
+    blobStyle.fill = color;
+    blobStyle.stroke = color;
+  }
+
+  async function init() {
+    const theme = await getTheme();
+    const themes = {
+      standard: colorArrays[0],
+      classic: colorArrays[1],
+      githubDark: colorArrays[2],
+      halloween: colorArrays[3],
+      teal: colorArrays[4],
+      leftPad: colorArrays[5],
+      dracula: colorArrays[6],
+      blue: colorArrays[7],
+      panda: colorArrays[8],
+      flame: colorArrays[9],
+      pink: colorArrays[10],
+      YlGnBu: colorArrays[11],
+      solarizedDark: colorArrays[12],
+      solarizedLight: colorArrays[13],
+    };
+
+    const colors = themes[theme] || [];
+    applyTheme(colors);
+  }
+
+  chrome.storage.onChanged.addListener((changes) => {
+    if (changes.favoriteColor) {
+      init();
+    }
+  });
+
+  // Initialize on page load
+  init();
+})();
+
+const selectButton = document.querySelector("#themeButton");
+const applyButton = document.querySelector("#setButton");
+
 function displayRadioValue() {
   var ele = document.getElementsByName("theme");
 
@@ -17,33 +111,34 @@ function displayRadioValue() {
 function currentSelectedTheme() {
   let currentTheme = document.getElementById("result").innerHTML;
   return currentTheme.split(":")[1].trim();
+  // console.log(currentTheme);
 }
+
+const githubUrl = "github.com";
+
+const reload = () => {
+  chrome.tabs.query({ url: `*://${githubUrl}/*` }, (tabs) => {
+    tabs.forEach((tab) => {
+      chrome.tabs.reload(tab.id);
+    });
+  });
+};
 
 /**
  * This function saves the selected theme to the chrome storage.
  */
 function savingSelectedColor() {
   let themeValue = currentSelectedTheme();
-  chrome.storage.sync.set(
-    {
-      favoriteColor: themeValue,
-    },
-    function () {
-      console.log("Value is set to " + themeValue);
-    }
-  );
+  chrome.storage.local.set({
+    favoriteColor: themeValue,
+  });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector("#themeButton");
-  button.addEventListener("click", () => {
-    displayRadioValue();
-  });
+selectButton.addEventListener("click", () => {
+  displayRadioValue();
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const buttonTwo = document.querySelector("#setButton");
-  buttonTwo.addEventListener("click", () => {
-    savingSelectedColor();
-  });
+applyButton.addEventListener("click", () => {
+  savingSelectedColor();
+  // reload();
 });
