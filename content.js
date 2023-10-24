e059767a1f6cf4c54acb93ad5d358fe7e2bdf7ce
rcmtcristian <rcmtcristian@gmail.com>
2023-10-23 21:40:41 -0400

    🎨 Changed the themes and struct
    added the heatmap theme and rainbow theme for now

--- content.js
+++ content.js
@@ -20,6 +20,17 @@
     const color = colors[colors.length - 1];
     blobStyle.fill = color;
     blobStyle.stroke = color;
+    //make it based on the first number of the width instead
+    // for each width, change the color of the progress bar
+    // loop through the li in the ul and get the width of each
+    // if width is 25%, change the color of the progress bar to the first color
+    // if width is 50%, change the color of the progress bar to the second color
+    // if width is 75%, change the color of the progress bar to the third color
+    // if width is 100%, change the color of the progress bar to the fourth color
+    const progressbarStyle =
+      document.getElementsByClassName(".Progress-item").style;
+    progressbarStyle.backgroundColor = "red";
+    // progressbarStyle.setProperty("background-color", color);
   }
 
   async function init() {
@@ -30,7 +41,7 @@
       githubDark: ["#101217", "#005e35", "#00844a", "#00aa5f", "#00d074"],
       halloween: ["#212836", "#631c03", "#bd561d", "#fa7a18", "#fddf68"],
       teal: ["#044c46", "#098379", "#46e8d8", "#9cfcf0", "#b8fff7"],
-      grayscale: ["#181818", "#646464", "#A5A5A5", "#DDDDDD", "#F6F6F6"],
+      rainbow: ["#ee761cyes", "#08a957", "#fff00d", "#363997", "#ee222b"],
       dracula: ["#49006a", "#ac0279", "#f669a0", "#faa0ba", "#fcc5c0"],
       blue: ["#eeeeee", "#bbdefb", "#64b5f6", "#1e88e5", "#0d47a1"],
       panda: ["#9c1b5b", "#ca1a50", "#f37651", "#f6bf9d", "#faebdd"],
@@ -51,18 +62,7 @@
       //   "#F74046",
       //   "#FB5860",
       // ],
-      // rainbow: [
-      //   "#FFFFFF",
-      //   "#3A86FF",
-      //   "#5F5FF6",
-      //   "#8338EC",
-      //   "#A22ACD",
-      //   "#85607",
-      //   "#FD2B3B",
-      //   "#FD8A09",
-      //   "#FF006E",
-      //   "#FFBEOB",
-      // ],
+      heatmap: ["#2aa8f2", "#8bd448", "#fae442", "#fba949", "#ff6355"],
     };
 
     const colors = themes[theme] || [];
