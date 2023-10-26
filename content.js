f47284acf9eaf3e41ea0ef7296d8ad657a56d28d
rcmtcristian <rcmtcristian@gmail.com>
2023-10-26 15:35:04 -0400

    🔖 Release ver 0-1-3

--- content.js
+++ content.js
@@ -20,17 +20,47 @@
     const color = colors[colors.length - 1];
     blobStyle.fill = color;
     blobStyle.stroke = color;
+    //make it based on the first number of the width instead
+    // for each width, change the color of the progress bar
+    // loop through the li in the ul and get the width of each
+    // if width is 25%, change the color of the progress bar to the first color
+    // if width is 50%, change the color of the progress bar to the second color
+    // if width is 75%, change the color of the progress bar to the third color
+    // if width is 100%, change the color of the progress bar to the fourth color
+    const progressbarStyle =
+      document.getElementsByClassName(".Progress-item").style;
+    progressbarStyle.backgroundColor = "red";
+    // progressbarStyle.setProperty("background-color", color);
   }
 
   async function init() {
+    const randomTheme = [
+      "#ffbe0b",
+      "#fd8a09",
+      "#fb5607",
+      "#fd2b3b",
+      "#c11cad",
+      "#a22acd",
+      "#83d483",
+      "#06d6a0",
+      "#118ab2",
+      "#0c637f ",
+    ];
+
     const theme = await getTheme();
+    const numberOfColors = randomTheme.length;
+    const rainbowTheme = Array.from(
+      { length: numberOfColors },
+      () => randomTheme[Math.floor(Math.random() * numberOfColors)]
+    );
+
     const themes = {
       standard: ["", "", "", "", ""],
       classic: ["#eeeeee", "#dcedc8", "#aed581", "#7cb342", "#33691e"],
       githubDark: ["#101217", "#005e35", "#00844a", "#00aa5f", "#00d074"],
       halloween: ["#212836", "#631c03", "#bd561d", "#fa7a18", "#fddf68"],
       teal: ["#044c46", "#098379", "#46e8d8", "#9cfcf0", "#b8fff7"],
-      grayscale: ["#181818", "#646464", "#A5A5A5", "#DDDDDD", "#F6F6F6"],
+      random: rainbowTheme,
       dracula: ["#49006a", "#ac0279", "#f669a0", "#faa0ba", "#fcc5c0"],
       blue: ["#eeeeee", "#bbdefb", "#64b5f6", "#1e88e5", "#0d47a1"],
       panda: ["#9c1b5b", "#ca1a50", "#f37651", "#f6bf9d", "#faebdd"],
@@ -39,30 +69,7 @@
       sunny: ["#eeeeee", "#fff9c4", "#fff176", "#ffd835", "#f57f17"],
       solarizedD: ["#eeeeee", "#fed800", "#ff6f01", "#fd2f24", "#811d5e"],
       solarizedL: ["#fdf6e3", "#eee8d5", "#839496", "#586e75", "#073642"],
-      // tritanopia: [
-      //   "#3DCAC5",
-      //   "#5FB4AE",
-      //   "#7F9E96",
-      //   "#E867F",
-      //   "#B86E68",
-      //   "#F2292C",
-      //   "#F23433",
-      //   "#F23E39",
-      //   "#F74046",
-      //   "#FB5860",
-      // ],
-      // rainbow: [
-      //   "#FFFFFF",
-      //   "#3A86FF",
-      //   "#5F5FF6",
-      //   "#8338EC",
-      //   "#A22ACD",
-      //   "#85607",
-      //   "#FD2B3B",
-      //   "#FD8A09",
-      //   "#FF006E",
-      //   "#FFBEOB",
-      // ],
+      heatmap: ["#2aa8f2", "#8bd448", "#fae442", "#fba949", "#ff6355"],
     };
 
     const colors = themes[theme] || [];
